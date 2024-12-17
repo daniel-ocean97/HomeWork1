102ca2be53dbffbf92868607c7d87986abff8ae2
Даниил Ермилов <yndx-daniel-ocean@yandex.ru>
2024-12-17 17:27:06 +0300

    add func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,9 @@
-def filter_by_state(data: list[dict], state: str='EXECUTED') -> list[dict]:
+def filter_by_state(client_data: list[dict], state: str= 'EXECUTED') -> list[dict]:
     """Функция фильтрует список по указанному значение state"""
-    return [i for i in data if i['state'] == state]
+    return [i for i in client_data if i['state'] == state]
 
 
-data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-
+def sort_by_date(client_data: list[dict], sort_order: bool=True) -> list[dict]:
+    """Функция сортирует список словарей по дате"""
+    return sorted(client_data, key=lambda x: x["date"], reverse=sort_order)
 
