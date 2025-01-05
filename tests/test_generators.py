d1cb1e60c7382a0a6b8012bfa14d8b76b5d6e211
Даниил Ермилов <yndx-daniel-ocean@yandex.ru>
2025-01-06 00:19:07 +0300

    add parametrise to generator tests

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,4 +1,5 @@
 import src.generators
+import pytest
 
 
 def test_filter_by_currency_usd(trans_data):
@@ -71,3 +72,38 @@ def test_card_number_generator_wrong():
     """Функция для тестирования card_number_generator если переданы неверные рамки"""
     result = src.generators.card_number_generator(0, 10)
     assert result == "Введены некорректные рамки"
+
+
+@pytest.mark.parametrize(
+    "data",
+    "expected",
+    [
+        (
+            {
+                "id": 873106923,
+                "state": "EXECUTED",
+                "date": "2019-03-23T01:09:46.296404",
+                "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+                "description": "Перевод со счета на счет",
+                "from": "Счет 44812258784861134719",
+                "to": "Счет 74489636417521191160",
+            },
+            "Перевод со счета на счет",
+        ),
+        (
+            {
+                "id": 895315941,
+                "state": "EXECUTED",
+                "date": "2018-08-19T04:27:37.904916",
+                "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+                "description": "Перевод с карты на карту",
+                "from": "Visa Classic 6831982476737658",
+                "to": "Visa Platinum 8990922113665229",
+            },
+            "Перевод с карты на карту",
+        ),
+    ],
+)
+def transaction_descriptions_one(data, expected):
+    actual_descriptions = list(src.generators.transaction_descriptions(data))
+    assert actual_descriptions == expected
