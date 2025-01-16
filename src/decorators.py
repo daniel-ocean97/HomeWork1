f547c8ec217c5c18e2f767372fab0c3dc88f69e8
Даниил Ермилов <yndx-daniel-ocean@yandex.ru>
2025-01-16 18:23:22 +0300

    add decorators module

---
+++ src/decorators.py
@@ -0,0 +1,29 @@
+from functools import wraps
+
+
+def log(filename=None):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if filename is None:
+                try:
+                    result = func(*args, **kwargs)
+                    print(f"{func.__name__} ok\n")
+                    return result
+                except Exception as e:
+                    print(f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}/n")
+            else:
+                try:
+                    result = func(*args, **kwargs)
+                    with open(filename, 'a', encoding="utf-8") as f:
+                        message = f"{func.__name__} ok\n"
+                        f.write(message)
+                    return result
+                except Exception as e:
+                    with open(filename, 'a', encoding="utf-8") as f:
+                        message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}\n"
+                        f.write(message)
+        return wrapper
+    return decorator
+
+
