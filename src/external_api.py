969219d91bebec24e712ef3d4f05567855efc26e
Даниил Ермилов <yndx-daniel-ocean@yandex.ru>
2025-01-24 15:00:22 +0300

    change API

--- src/external_api.py
+++ src/external_api.py
@@ -13,12 +13,12 @@ def show_amount_in_rub(data):
     if currency == "RUB":
         return float(amount)
     else:
-        # Пришлось использовать другой API с этого же сервиса, то, на который была ссылка в условиях домашки не отвечал
-        # Currency Data API - название API
-        url = f"https://api.apilayer.com/currency_data/convert?to=RUB&from={currency}&amount={amount}"
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
 
         payload = {}
-        headers = {"apikey": f"{token}"}
+        headers = {
+            "apikey": f"{token}"
+        }
         response = requests.request("GET", url, headers=headers, data=payload)
 
         result = response.json()
